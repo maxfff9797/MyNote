6a039aefd501926522a96ce45f9fa95e454e86be
Fengfanfan <fengfanfan@Fengfanfans-mac.local>
2017-08-15 11:20:48 -0700

    0816-1120

---
+++ Aug16.mk
@@ -0,0 +1,13 @@
+Vlantin str
+16th str
+
+#Innovation
+Innovation is an iterative process of invention and the resulting total distribution of outcomes. 
+##Measure Innovation
+·Shot at goal
+·Average score
+·Maximum scores
+
+#Startup
+
+#Join starup
---
+++ Aug8.mk
@@ -0,0 +1,11 @@
+# Data Science
+## import
+retrieving and downloading
+## clean
+·improve data more useful.
+·fingerout good or throw.
+## 
+## model
+use your data to build model and test model
+## visualiza
+pic is easy to understand
